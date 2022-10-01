56f9aeeb0979c7e5d6154c31202099c7970b775c
Sebastian Weddmark Olsson <visnae@gmail.com>
2022-10-01 12:50:36 +0200

    symmetric tcap decoding

--- src/otc_tcap.erl
+++ src/otc_tcap.erl
@@ -13,8 +13,7 @@ spec() ->
     "ITU-T Q.773 (06/97)".
 
 next(#{dialoguePortion :=
-           #{'application-context-name' := {0,4,0,0,1,0,21,3},
-             'protocol-version' := [version1]}}) -> {ok, map};
+           #{'application-context-name' := {0,4,0,0,1,0,21,3}}}) -> {ok, map};
 next(_) -> '$stop'.
 
 codec(Bin) when is_binary(Bin) ->
@@ -24,71 +23,114 @@ codec(Map) when is_map(Map) ->
     encode(Map).
 
 decode(Bin) ->
-    {ok, {Type, #{dialoguePortion := Dialogue} = TC0}} = 'TCAPMessages-simple':decode('TCMessage', Bin),
-    {ok, {DiaType, Dia0}} = decode_dialogue(Dialogue),
-    TC1 = maps:without([dialoguePortion], TC0),
-    TC = parse_transaction_ids(TC1),
-    Dia = parse_application_context_name(Dia0),
-    TC#{message_type => Type,
-        dialogue_type => DiaType,
-        dialogue_portion => Dia
-       }.
+    {ok, D} = 'TCAPMessages-simple':decode('TCMessage', Bin),
+    decode_message(D).
 
-encode(#{message_type := Type} = Map) ->
-    Dia = encode_dialogue(Map),
-    Msg = compose_transaction_ids(Dia),
-    'TCAPMessages-simple':encode('TCMessage', {Type, Msg}).
+encode(Map) ->
+    TC = encode_message(Map),
+    {ok, Bin} = 'TCAPMessages-simple':encode('TCMessage', TC),
+    Bin.
 
-parse_transaction_ids(#{dtid := DT, otid := OT} = M) ->
-    M#{dtid => binary:encode_hex(DT),
-       otid => binary:encode_hex(OT)};
-parse_transaction_ids(#{dtid := DT} = M) ->
-    M#{dtid => binary:encode_hex(DT)};
-parse_transaction_ids(#{otid := OT} = M) ->
-    M#{otid => binary:encode_hex(OT)};
-parse_transaction_ids(M) ->
-    M.
+decode_message({'begin', #{otid := OT} = TC}) ->
+    TCDialogue = decode_dialogue(TC),
+    TCDialogue#{type => 'begin',
+                otid => binary:encode_hex(OT)};
+decode_message({'continue', #{otid := OT, dtid := DT} = TC}) ->
+    TCDialogue = decode_dialogue(TC),
+    TCDialogue#{type => 'continue',
+                otid => binary:encode_hex(OT),
+                dtid => binary:encode_hex(DT)};
+decode_message({'end', #{dtid := DT} = TC}) ->
+    TCDialogue = decode_dialogue(TC),
+    TCDialogue#{type => 'end',
+                dtid => binary:encode_hex(DT)};
+decode_message({'abort', #{dtid := DT} = TC}) ->
+    TC#{type => 'abort',
+        dtid => binary:encode_hex(DT)}.
 
-compose_transaction_ids(#{dtid := DT, otid := OT} = M) ->
-    M#{dtid => binary:decode_hex(DT),
-       otid => binary:decode_hex(OT)};
-compose_transaction_ids(#{dtid := DT} = M) ->
-    M#{dtid => binary:decode_hex(DT)};
-compose_transaction_ids(#{otid := OT} = M) ->
-    M#{otid => binary:decode_hex(OT)};
-compose_transaction_ids(M) ->
-    M#{}.
+encode_message(#{type := 'begin', otid := OT} = Map) ->
+    Dialogue = encode_dialogue(Map),
+    {'begin', #{dialoguePortion => Dialogue,
+                otid => binary:decode_hex(OT)}};
+encode_message(#{type := 'continue', otid := OT, dtid := DT} = Map) ->
+    Dialogue = encode_dialogue(Map),
+    {'continue', #{dialoguePortion => Dialogue,
+                   otid => binary:decode_hex(OT),
+                   dtid => binary:decode_hex(DT)}};
+encode_message(#{type := 'end', dtid := DT} = Map) ->
+    Dialogue = encode_dialogue(Map),
+    {'end', #{dialoguePortion => Dialogue,
+              dtid => binary:decode_hex(DT)}};
+encode_message(#{type := 'abort', dtid := DT} = Map) ->
+    Dialogue = encode_dialogue(Map),
+    {'abort', #{dialoguePortion => Dialogue,
+                   dtid => binary:decode_hex(DT)}}.
 
-decode_dialogue(#{'direct-reference' := DR, encoding := {'single-ASN1-type', Enc}}) ->
+decode_dialogue(#{dialoguePortion := #{'direct-reference' := DR, encoding := {'single-ASN1-type', Enc}}}) ->
     UniID = 'UnidialoguePDUs':'uniDialogue-as-id'(),
     DiaID = 'DialoguePDUs':'dialogue-as-id'(),
-    case DR of
-        UniID ->
-            'UnidialoguePDUs':decode('UniDialoguePDU', Enc);
-        DiaID ->
-            'DialoguePDUs':decode('DialoguePDU', Enc)
-    end.
+    {ok, {DiaType, Dia0}} = case DR of
+                                UniID ->
+                                    'UnidialoguePDUs':decode('UniDialoguePDU', Enc);
+                                DiaID ->
+                                    'DialoguePDUs':decode('DialoguePDU', Enc)
+                            end,
+    ACN = parse_application_context_name(Dia0),
+    #{dialogue_type => DiaType,
+      application_context_name => ACN}.
 
-encode_dialogue(#{dialogue_type := DiaType, dialogue_portion := Dia} = Map) ->
+encode_dialogue(#{dialogue_type := DiaType} = Map) ->
+    ACN = compose_application_context_name(Map),
+    DiaID = 'DialoguePDUs':'dialogue-as-id'(),
+    UniID = 'UnidialoguePDUs':'uniDialogue-as-id'(),
     case DiaType of
         dialogueRequest ->
-            DiaID = 'DialoguePDUs':'dialogue-as-id'(),
-            {ok, Enc} = 'DialoguePDUs':encode('DialoguePDU', Dia),
-            Map#{dialoguePortion =>
-                     #{'direct-reference' => DiaID,
-                       encoding => {'single-ASN1-type', Enc}}}
+            Dia = #{'protocol-version' => [version1],
+                    'application-context-name' => ACN
+                    %% 'user-information' =>
+                   },
+            {ok, Enc} = 'DialoguePDUs':encode('DialoguePDU', {DiaType, Dia}),
+            #{'direct-reference' => DiaID,
+              encoding => {'single-ASN1-type', Enc}};
+        dialogueResponse ->
+            Dia = #{'protocol-version' => [version1],
+                    'application-context-name' => ACN
+                    %% 'result' => ,
+                    %% 'result-source-diagnostic' => ,
+                    %% 'user-information' =>
+                   },
+            {ok, Enc} = 'DialoguePDUs':encode('DialoguePDU', {DiaType, Dia}),
+            #{'direct-reference' => DiaID,
+              encoding => {'single-ASN1-type', Enc}};
+        dialogueAbort ->
+            Dia = #{%% 'abort-source' => ,
+                    %% 'user-information' =>
+                   },
+            {ok, Enc} = 'DialoguePDUs':encode('DialoguePDU', {DiaType, Dia}),
+            #{'direct-reference' => DiaID,
+              encoding => {'single-ASN1-type', Enc}};
+        unidialoguePDU ->
+            Dia = #{'protocol-version' => [version1],
+                    'application-context-name' => ACN
+                    %% 'user-information' =>
+                   },
+            {ok, Enc} = 'UnidialoguePDUs':encode('UnidialoguePDU', {DiaType, Dia}),
+            #{'direct-reference' => UniID,
+              encoding => {'single-ASN1-type', Enc}}
     end.
 
 parse_application_context_name(#{'application-context-name' := ACN}) ->
-    MAP = tuple_to_list('MAP-ApplicationContexts':'map-ac'()),
-    case lists:prefix(MAP, tuple_to_list(ACN)) of
-        true ->
-            parse_map_application_context_name(ACN)
-    end.
-
-parse_map_application_context_name(ACN) ->
-    ACNs = [{'MAP-ApplicationContexts':F(), F} || {F, 0} <- 'MAP-ApplicationContexts':module_info(exports)],
+    ACNs = [{'MAP-ApplicationContexts':F(), F}
+            || {F, 0} <- 'MAP-ApplicationContexts':module_info(exports)],
     case lists:keyfind(ACN, 1, ACNs) of
         false -> ACN;
         {_, F} -> F
+    end.
+
+compose_application_context_name(#{application_context_name := ACN}) ->
+    ACNs = [{'MAP-ApplicationContexts':F(), F}
+            || {F, 0} <- 'MAP-ApplicationContexts':module_info(exports)],
+    case lists:keyfind(ACN, 2, ACNs) of
+        false -> ACN;
+        {F, _} -> F
     end.
