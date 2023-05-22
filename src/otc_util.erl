561d1e83b80bd1842adf74fd2fec336ad3515d56
Mikael Lixenstrand <lixen@wgtwo.com>
2023-05-22 16:08:42 +0200

    add spec ref and descriptive name

--- src/otc_util.erl
+++ src/otc_util.erl
@@ -3,8 +3,8 @@
 -export([
     decode_tbcd/1,
     encode_tbcd/1,
-    decode_mcc_mnc/1,
-    encode_mcc_mnc/1
+    decode_common_mcc_mnc/1,
+    encode_common_mcc_mnc/1
 ]).
 
 
@@ -52,14 +52,15 @@ enc_tbcd_digit(B) when B =:= $B; B =:= $b -> 2#1101;
 enc_tbcd_digit(C) when C =:= $C; C =:= $c -> 2#1110;
 enc_tbcd_digit(F) when F =:= $F; F =:= $f -> 2#1111.
 
-decode_mcc_mnc(<<MCC2:4, MCC1:4, 2#1111:4, MCC3:4, MNC2:4, MNC1:4>>) ->
+%% 3GPP TS 24.008 
+decode_common_mcc_mnc(<<MCC2:4, MCC1:4, 2#1111:4, MCC3:4, MNC2:4, MNC1:4>>) ->
     lists:map(fun dec_tbcd_digit/1, [MCC1, MCC2, MCC3, MNC1, MNC2]);
-decode_mcc_mnc(<<MCC2:4, MCC1:4, MNC3:4, MCC3:4, MNC2:4, MNC1:4>>) ->
+decode_common_mcc_mnc(<<MCC2:4, MCC1:4, MNC3:4, MCC3:4, MNC2:4, MNC1:4>>) ->
     lists:map(fun dec_tbcd_digit/1, [MCC1, MCC2, MCC3, MNC1, MNC2, MNC3]).
 
-encode_mcc_mnc(MCCMNC) when length(MCCMNC) == 5 ->
+encode_common_mcc_mnc(MCCMNC) when length(MCCMNC) == 5 ->
     [MCC1, MCC2, MCC3, MNC1, MNC2] = lists:map(fun enc_tbcd_digit/1, MCCMNC),
     <<MCC2:4, MCC1:4, 2#1111:4, MCC3:4, MNC2:4, MNC1:4>>;
-encode_mcc_mnc(MCCMNC) when length(MCCMNC) == 6 ->
+encode_common_mcc_mnc(MCCMNC) when length(MCCMNC) == 6 ->
     [MCC1, MCC2, MCC3, MNC1, MNC2, MNC3] = lists:map(fun enc_tbcd_digit/1, MCCMNC),
     <<MCC2:4, MCC1:4, MNC3:4, MCC3:4, MNC2:4, MNC1:4>>.
