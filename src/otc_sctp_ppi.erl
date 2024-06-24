ba888d4ccde3b2f810637081c51c58e4d690ca35
Sebastian Weddmark Olsson <visnae@gmail.com>
2024-06-24 16:35:44 +0200

    [sctp] encode/decode

--- src/otc_sctp_ppi.erl
+++ src/otc_sctp_ppi.erl
@@ -20,8 +20,26 @@ codec(PPI) when is_integer(PPI) ->
 codec(PPI) when is_atom(PPI) ->
     encode(PPI).
 
+decode(?SCTP_PPI_M2UA) -> m2ua;
 decode(?SCTP_PPI_M3UA) -> m3ua;
-decode(?SCTP_PPI_M2PA) -> m2pa.
+decode(?SCTP_PPI_M2PA) -> m2pa;
+decode(?SCTP_PPI_S1AP) -> s1ap;
+decode(?SCTP_PPI_M2AP) -> m2ap;
+decode(?SCTP_PPI_M3AP) -> m3ap;
+decode(?SCTP_PPI_SSH) -> ssh;
+decode(?SCTP_PPI_DIAMETER) -> diameter;
+decode(?SCTP_PPI_DIAMETER_DTLS) -> diameter_dtls;
+decode(?SCTP_PPI_NGAP) -> ngap;
+decode(?SCTP_PPI_NGAP_DTLS) -> ngap_dtls.
 
+encode(m2ua) -> ?SCTP_PPI_M2UA;
 encode(m3ua) -> ?SCTP_PPI_M3UA;
-encode(m2pa) -> ?SCTP_PPI_M2PA.
+encode(m2pa) -> ?SCTP_PPI_M2PA;
+encode(s1ap) -> ?SCTP_PPI_S1AP;
+encode(m2ap) -> ?SCTP_PPI_M2AP;
+encode(m3ap) -> ?SCTP_PPI_M3AP;
+encode(ssh) -> ?SCTP_PPI_SSH;
+encode(diameter) -> ?SCTP_PPI_DIAMETER;
+encode(diameter_dtls) -> ?SCTP_PPI_DIAMETER_DTLS;
+encode(ngap) -> ?SCTP_PPI_NGAP;
+encode(ngap_dtls) -> ?SCTP_PPI_NGAP_DTLS.
